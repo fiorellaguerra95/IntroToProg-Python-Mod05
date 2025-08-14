de9a7b21e54e6cf57a9fe8f433a976726a423d41
fiorellaguerra95 <fguerra1@uw.edu>
2025-08-13 23:45:52 -0700

    Add files via upload

---
+++ Assignment05.py
@@ -0,0 +1,143 @@
+# --------------------------------------------------------------------------- #
+# Title: Assignment05
+# Desc: This assignment demonstrates using dictionaries, files, and exception
+# handling
+# Change Log: (Who, When, What)
+#   RRoot,1/1/2030,Created Script
+#   Fiorella Guerra, 08/13/2025, Created Script
+# --------------------------------------------------------------------------- #
+
+import json
+
+# Define the Data Constants
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+# Define the Data Constants
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables and constants
+# Holds the first name of a student entered by the user.
+student_first_name: str = ''
+# Holds the last name of a student entered by the user.
+student_last_name: str = ''
+course_name: str = ''  # Holds the name of a course entered by the user.
+student_data: dict = {}  # one row of student data
+students: list = []  # a table of student data
+# Holds combined CSV data. Note: Remove later since it is NOT needed with the JSON File
+file = None  # Holds a reference to an opened file.
+menu_choice: str = ''  # Hold the choice made by the user.
+
+
+# When the program starts, read the file data into a list of lists (table)
+# Extract the data from the file
+try:
+    file = open(FILE_NAME, "r")
+    students = json.load(file)
+    if not isinstance(students, list):
+        raise ValueError("File does not contain a list of student records.")
+
+except FileNotFoundError as e:
+    print("Text file must exist before running this script! Starting with \
+        an empty list.\n")
+    print("Built-In Python error info: ")
+    print(e, e.__doc__, type(e), sep='\n')
+    students = []
+
+except json.JSONDecodeError as e:
+    print("Please check that the data is a valid JSON format\n")
+    print("-- Technical Error Message -- ")
+    print(e, e.__doc__, type(e), sep='\n')
+
+except Exception as e:
+    print("-- Technical Error Message -- ")
+    print("Built-In Python error info: ")
+    print(e, e.__doc__, type(e), sep='\n')
+
+finally:
+    if file and file.closed == False:
+        file.close()
+
+
+# Present and Process the data
+while (True):
+
+    # Present the menu of choices
+    print(MENU)
+    menu_choice = input("What would you like to do: ")
+
+    # Input user data
+    if menu_choice == "1":  # This will not work if it is an integer!
+        try:
+            student_first_name = input("Enter the student's first name: ")
+            if not student_first_name.isalpha():
+                raise ValueError("The first name should not contain numbers.")
+        except ValueError as e:
+            print("Invalid input:", e)
+            continue  # Go back to main menu
+        try:
+            student_last_name = input("Enter the student's last name: ")
+            if not student_last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+        except ValueError as e:
+            print("Invalid input:", e)
+            continue  # Go back to main menu
+        course_name = input("Please enter the name of the course: ")
+        student_data = {
+            "FirstName": student_first_name,
+            "LastName": student_last_name,
+            "CourseName": course_name
+        }
+        students.append(student_data)
+        print("You have registered", student_first_name,
+              student_last_name, "for", course_name)
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+
+        # Process the data to create and display a custom message
+        print("-"*50)
+        for student in students:
+            print(",".join(student.values()))
+        print("-"*50)
+        continue
+
+    # Save the data to a file
+    # Note: On Linux and macOS, having the file open in another program
+    # may not trigger PermissionError unless the file is actively locked
+    # or you don't have write permissions.
+    elif menu_choice == "3":
+        try:
+            file = open(FILE_NAME, 'w')
+            json.dump(students, file)
+            file.close()
+            for student in students:
+                print("Student", student['FirstName'], student['LastName'],
+                      "is enrolled in", student['CourseName'])
+
+        except PermissionError:
+            print("Please close the file before saving.")
+
+        except TypeError:
+            print("Data contains values that cannot be saved to JSON.")
+
+        except OSError as e:
+            print("File system error while saving:", e)
+
+        except Exception as e:
+            print("Unexpected error while saving:", e)
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+    else:
+        print("Please only choose option 1, 2, 3, or 4")
+
+print("Program Ended")
